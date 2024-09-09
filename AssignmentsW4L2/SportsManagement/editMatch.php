f604c5cca4bb74c3cdcd6b0c0c1e2fe9bc920e0e
SevOps <fros7deagl3@gmail.com>
2024-09-09 20:30:27 +0300

    finishing up the editMatch functionality (its total hell)

--- AssignmentsW4L2/SportsManagement/editMatch.php
+++ AssignmentsW4L2/SportsManagement/editMatch.php
@@ -8,21 +8,19 @@ if (!isset($_SESSION['username'])) {
 }
 
 if(isset($_POST['submit'])) {
-    print_r($_POST);
-
+    $id = $_GET['id'];
     $team1ID = $_POST['team1'];
     $team2ID = $_POST['team2'];
     $team1Goals = $_POST['team1goals'];
     $team2Goals = $_POST['team2goals'];
     $date = $_POST['matchDate'];
 
-    $query = "INSERT INTO matches (id, team1ID, team2ID, team1goals, team2goals, matchDate) 
-              VALUES (NULL, '$team1ID', '$team2ID', '$team1Goals', '$team2Goals', '$date')";
+    $query = "UPDATE matches SET team1ID = '$team1ID', team2ID = '$team2ID', team1goals = '$team1Goals', team2goals = '$team2Goals', matchDate = '$date' where id = '$id'";
     $resultInsert = mysqli_query($conn, $query);
     if ($resultInsert) {
-        header('Location: matches.php?msg=Match Created');
+        header('Location: matches.php?msg=Match Updated');
     }else{
-        echo "Failed creating player" . mysqli_error($conn);
+        echo "Failed updating player" . mysqli_error($conn);
     }
 
 }
@@ -72,32 +70,43 @@ $result = mysqli_query($conn, $searchQuery);
 $searchQuery2 = 'SELECT * FROM teams';
 $result2 = mysqli_query($conn, $searchQuery);
 
-
+$id = $_GET['id'];
+$editQuery = "SELECT * FROM matches where id = '$id'";
+$editResult = mysqli_query($conn, $editQuery);
+$editRow = mysqli_fetch_assoc($editResult);
 
 ?>
 <form method="post" class="flex-col flex">
     <label for="team1">Team 1</label>
     <select name="team1" id="team1">
         <?php while ($rows = mysqli_fetch_assoc($result)) {
-            echo "<option value=" . $rows['id'] . ">" . $rows['teamName'] . "</option>";
+            if ($editRow['team1ID'] === $rows['id']) {
+                echo "<option selected value=" . $rows['id'] . ">" . $rows['teamName'] . "</option>";
+            }else {
+                echo "<option value=" . $rows['id'] . ">" . $rows['teamName'] . "</option>";
+            }
         }?>
     </select>
     <label for="team2">Team 2</label>
     <select name="team2" id="team2">
         <?php while ($rows = mysqli_fetch_assoc($result2)) {
-            echo "<option value=" . $rows['id'] . ">" . $rows['teamName'] . "</option>";
+            if ($editRow['team2ID'] === $rows['id']) {
+                echo "<option selected value=" . $rows['id'] . ">" . $rows['teamName'] . "</option>";
+            }else {
+                echo "<option value=" . $rows['id'] . ">" . $rows['teamName'] . "</option>";
+            }
         }?>
     </select>
     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
         Team 1 goals
     </label>
-    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-age" type="number" name="team1goals" placeholder="Enter goals for Team 1">
+    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-age" type="number" name="team1goals" value="<?php echo $editRow['team1goals']?>">
     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
         Team 2 goals
     </label>
-    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-age" type="number" name="team2goals" placeholder="Enter goals for Team 2">
-    <input type="date" name="matchDate">
-    <button class="bg-black text-white p-5 rounded-md" type="submit" name="submit">Create Match</button>
+    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-age" type="number" name="team2goals" value="<?php echo $editRow['team2goals']?>">
+    <input type="date" name="matchDate" value="<?php echo $editRow['matchDate'] ?>">
+    <button class="bg-black text-white p-5 rounded-md" type="submit" name="submit">Update Match</button>
 </form>
 </body>
 </html>
