4fde4e385118aac174da63be1505d7e2b4bb4a35
SevOps <72295557+severin29@users.noreply.github.com>
2024-09-12 17:49:14 +0300

    Update dashboard.php (adding stats)

--- AssignmentsW4L2/SportsManagement/dashboard.php
+++ AssignmentsW4L2/SportsManagement/dashboard.php
@@ -1,7 +1,7 @@
 <?php
 include 'db.php';
-    session_start();
-    $_SESSION['username'];
+session_start();
+$_SESSION['username'];
 if (!isset($_SESSION['username'])) {
     header('Location: loginPage.php');
     exit();
@@ -44,6 +44,198 @@ if (!isset($_SESSION['username'])) {
         </div>
     </div>
 </nav>
+<div class="mx-auto">
+    <div class="relative flex flex-col w-full h-full overflow-scroll text-slate-300 bg-slate-800 shadow-md rounded-lg bg-clip-border">
+        <table class="w-full text-left table-auto min-w-max mx-10 my-10">
+            <thead>
+            <tr>
+                <th class="p-4 border-b border-slate-600 bg-slate-700">
+                    <p class="text-2xl font-normal leading-none text-slate-300">
+                        Players per team
+                    </p>
+                </th>
+                <th class="p-4 border-b border-slate-600 bg-slate-700">
+                    <p class="text-2xl font-normal leading-none text-slate-300">
+                        Value
+                    </p>
+                </th>
+            </thead>
+            <tbody>
+                <?php
 
+                $teamsQuery = "SELECT * FROM teams";
+                $teamsResult = mysqli_query($conn, $teamsQuery);
+
+                while ($teamRow = mysqli_fetch_assoc($teamsResult)) {
+                    $playersQuery = "SELECT COUNT(id) FROM players WHERE team_id = $teamRow[id]";
+                    $playersResult = mysqli_query($conn, $playersQuery);
+                    $playerRow = mysqli_fetch_assoc($playersResult);
+                    echo "<tr class='hover:bg-slate-700'>
+                                 <td class='p-4 border-b border-slate-700 text-lg'>" . $teamRow['teamName'] . "</td>
+                                 <td class='p-4 border-b border-slate-700 text-lg'>" . $playerRow['COUNT(id)'] . "</td>
+                              </tr>";
+                }
+
+                ?>
+            </tbody>
+        </table>
+        <hr>
+        <table class="w-full text-left table-auto min-w-max mx-10 my-10">
+            <thead>
+            <tr>
+                <th class="p-4 border-b border-slate-600 bg-slate-700">
+                    <p class="text-2xl font-normal leading-none text-slate-300">
+                        Average age of players per team
+                    </p>
+                </th>
+                <th class="p-4 border-b border-slate-600 bg-slate-700">
+                    <p class="text-2xl font-normal leading-none text-slate-300">
+                        Value
+                    </p>
+                </th>
+            </thead>
+            <tbody>
+            <?php
+
+            $teamsQuery = "SELECT * FROM teams";
+            $teamsResult = mysqli_query($conn, $teamsQuery);
+
+            while ($teamRow = mysqli_fetch_assoc($teamsResult)) {
+                $playersQuery = "SELECT AVG(age) FROM players WHERE team_id = $teamRow[id]";
+                $playersResult = mysqli_query($conn, $playersQuery);
+                $playerRow = mysqli_fetch_assoc($playersResult);
+                echo "<tr class='hover:bg-slate-700'>
+                                 <td class='p-4 border-b border-slate-700 text-lg'>" . $teamRow['teamName'] . "</td>
+                                 <td class='p-4 border-b border-slate-700 text-lg'>" . (int) $playerRow['AVG(age)'] . "</td>
+                              </tr>";
+            }
+
+            ?>
+            </tbody>
+        </table>
+        <hr>
+        <table class="w-full text-left table-auto min-w-max mx-10 my-10">
+            <thead>
+            <tr>
+                <th class="p-4 border-b border-slate-600 bg-slate-700">
+                    <p class="text-2xl font-normal leading-none text-slate-300">
+                        Matches Played by Team
+                    </p>
+                </th>
+                <th class="p-4 border-b border-slate-600 bg-slate-700">
+                    <p class="text-2xl font-normal leading-none text-slate-300">
+                        Value
+                    </p>
+                </th>
+            </thead>
+            <tbody>
+            <?php
+            $matchesQuery = "SELECT t.ID,
+                                        t.teamName,
+                                        COUNT(m.ID) AS matchesPlayed
+                                    FROM
+                                        teams t
+                                            LEFT JOIN
+                                        matches m ON t.ID = m.team1ID OR t.ID = m.team2ID
+                                    GROUP BY
+                                        t.ID, t.teamName
+                                    ORDER BY
+                                        matchesPlayed DESC";
+            $matchesResult = mysqli_query($conn, $matchesQuery);
+            while ($matchesRow = mysqli_fetch_assoc($matchesResult)) {
+                echo "<tr class='hover:bg-slate-700'>
+                                 <td class='p-4 border-b border-slate-700 text-lg'>" . $matchesRow['teamName'] . "</td>
+                                 <td class='p-4 border-b border-slate-700 text-lg'>" . $matchesRow['matchesPlayed'] . "</td>
+                              </tr>";
+
+            }
+
+            ?>
+            </tbody>
+        </table>
+        <hr>
+        <table class="w-full text-left table-auto min-w-max mx-10 my-10">
+            <thead>
+            <tr>
+                <th class="p-4 border-b border-slate-600 bg-slate-700">
+                    <p class="text-2xl font-normal leading-none text-slate-300">
+                        Average goals per team in a single match
+                    </p>
+                </th>
+                <th class="p-4 border-b border-slate-600 bg-slate-700">
+                    <p class="text-2xl font-normal leading-none text-slate-300">
+                        Value
+                    </p>
+                </th>
+            </thead>
+            <tbody>
+            <?php
+
+            $goalsQuery = "SELECT t.teamName,
+                                AVG(CASE
+                                        WHEN m.team1ID = t.ID THEN m.team1goals
+                                        ELSE m.team2goals
+                                    END) AS average_goals
+                            FROM
+                                matches m
+                                    JOIN
+                                teams t ON t.ID IN (m.team1ID, m.team2ID)
+                            GROUP BY
+                                t.teamName";
+            $goalsResult = mysqli_query($conn, $goalsQuery);
+            while ($goalsRow = mysqli_fetch_assoc($goalsResult)) {
+                echo "<tr class='hover:bg-slate-700'>
+                                 <td class='p-4 border-b border-slate-700 text-lg'>" . $goalsRow['teamName'] . "</td>
+                                 <td class='p-4 border-b border-slate-700 text-lg'>" . $goalsRow['average_goals'] . "</td>
+                              </tr>";
+            }
+            ?>
+
+            </tbody>
+        </table>
+        <hr>
+        <table class="w-full text-left table-auto min-w-max mx-10 my-10">
+            <thead>
+            <tr>
+                <th class="p-4 border-b border-slate-600 bg-slate-700">
+                    <p class="text-2xl font-normal leading-none text-slate-300">
+                        Ranking by Match Wins
+                    </p>
+                </th>
+                <th class="p-4 border-b border-slate-600 bg-slate-700">
+                    <p class="text-2xl font-normal leading-none text-slate-300">
+                        Value
+                    </p>
+                </th>
+            </thead>
+            <tbody>
+            <?php
+
+            $winsQuery = "SELECT t.teamName,
+                                COUNT(CASE
+                                          WHEN m.team1ID = t.ID AND m.team1Goals > m.team2Goals THEN 1
+                                          WHEN m.team2ID = t.ID AND m.team2Goals > m.team1Goals THEN 1
+                                    END) AS matchWins
+                            FROM
+                                teams t
+                                    LEFT JOIN
+                                matches m ON t.ID = m.team1ID OR t.ID = m.team2ID
+                            GROUP BY
+                                t.teamName
+                            ORDER BY matchWins DESC";
+
+            $winsResult = mysqli_query($conn, $winsQuery);
+            while ($winsRow = mysqli_fetch_assoc($winsResult)) {
+                echo "<tr class='hover:bg-slate-700'>
+                                 <td class='p-4 border-b border-slate-700 text-lg'>" . $winsRow['teamName'] . "</td>
+                                 <td class='p-4 border-b border-slate-700 text-lg'>" . $winsRow['matchWins'] . "</td>
+                              </tr>";
+            }
+            ?>
+
+            </tbody>
+        </table>
+    </div>
+</div>
 </body>
 </html>
