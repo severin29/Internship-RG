41a5ef94bdf1b64dba39df661a44541ec1d15804
SevOps <fros7deagl3@gmail.com>
2024-09-26 22:12:25 +0300

    adding tests for the entities

---
+++ AssignmentsW6L1/tests/CommentTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Entity;
+
+use App\Entity\Comment;
+use App\Entity\Post;
+use PHPUnit\Framework\TestCase;
+use App\Entity\User;
+class CommentTest extends TestCase
+{
+
+    public function testSetAuthor()
+    {
+        $comment = new Comment();
+        $user = new User();
+        $comment->setAuthor($user);
+        $this->assertSame($user, $comment->getAuthor());
+
+
+    }
+
+    public function testSetContent()
+    {
+        $comment = new Comment();
+        $comment->setContent('Test comment');
+        $this->assertSame('Test comment', $comment->getContent());
+    }
+
+    public function testGetAuthor()
+    {
+        $comment = new Comment();
+        $user = new User();
+        $comment->setAuthor($user);
+        $this->assertSame($user, $comment->getAuthor());
+    }
+
+    public function testGetPost()
+    {
+        $comment = new Comment();
+        $post = new Post();
+        $comment->setPost($post);
+        $this->assertSame($post, $comment->getPost());
+    }
+
+    public function testSetPost()
+    {
+        $comment = new Comment();
+        $post = new Post();
+        $comment->setPost($post);
+        $this->assertSame($post, $comment->getPost());
+    }
+
+    public function testGetId()
+    {
+
+    }
+
+    public function testGetContent()
+    {
+        $comment = new Comment();
+        $comment->setContent('Test comment');
+        $this->assertSame('Test comment', $comment->getContent());
+    }
+
+    public function testSetRating()
+    {
+        $comment = new Comment();
+        $comment->setRating(5);
+        $this->assertSame(5, $comment->getRating());
+    }
+
+    public function testGetRating()
+    {
+        $comment = new Comment();
+        $comment->setRating(5);
+        $this->assertSame(5, $comment->getRating());
+    }
+}
---
+++ AssignmentsW6L1/tests/PostTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\Post;
+use PHPUnit\Framework\TestCase;
+
+class PostTest extends TestCase
+{
+
+}
